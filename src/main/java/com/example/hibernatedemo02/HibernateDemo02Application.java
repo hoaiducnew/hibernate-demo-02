14854b0813b82b6baee01fc03caa27d0ce331c91
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-19 19:54:50 -0400

    initial commit

---
+++ src/main/java/com/example/hibernatedemo02/HibernateDemo02Application.java
@@ -0,0 +1,12 @@
+package com.example.hibernatedemo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDemo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDemo02Application.class, args);
+	}
+}
