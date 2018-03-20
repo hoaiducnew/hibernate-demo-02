0e712c3aa7e82f537476cc9e8f8f1ca7a07ec632
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-20 14:29:10 -0400

    70. Step 26 - OneToOne Mapping - Lazy Fetch - important

---
+++ src/test/java/com/example/hibernatedemo02/repository/StudentRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.hibernatedemo02.repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.hibernatedemo02.HibernateDemoApplication;
+import com.example.hibernatedemo02.entity.Student;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateDemoApplication.class)
+public class StudentRepositoryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	StudentRepository repository;
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	@Transactional // important
+	public void retrieveStudentAndPassportDetails() {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("student -> {}", student);
+		logger.info("passport -> {}", student.getPassport());
+	}
+}
