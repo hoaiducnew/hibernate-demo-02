51611d4baf3c3fa1a74fabc719475103b763853e
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-20 11:53:51 -0400

    62. Step 18 - JPA and Hibernate Annotations - @UpdateTimestamp & @CreationTimestamp

--- src/main/java/com/example/hibernatedemo02/repository/CourseRepository.java
+++ src/main/java/com/example/hibernatedemo02/repository/CourseRepository.java
@@ -38,6 +38,7 @@ public class CourseRepository {
 		Course course1 = new Course("Web Services in 100 Steps");
 		em.persist(course1);
 
-		course1.setName(null);
+		Course course2 = findById(10001L);
+		course2.setName("JPA in 50 Steps - Updated");
 	}
 }
