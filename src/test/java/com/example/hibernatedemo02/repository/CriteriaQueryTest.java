ebc58124bd091585b8c54526fb79fd3efb3d574e
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-21 19:12:19 -0400

    103. Step 54 - Criteria Query - Retrieving all courses

---
+++ src/test/java/com/example/hibernatedemo02/repository/CriteriaQueryTest.java
@@ -0,0 +1,56 @@
+package com.example.hibernatedemo02.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.hibernatedemo02.HibernateDemoApplication;
+import com.example.hibernatedemo02.entity.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateDemoApplication.class)
+public class CriteriaQueryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	public void all_courses() {
+		// "Select c From Course c"
+
+		// 1. Use Criteria Builder to create a Criteria Query returning the
+		// expected result object
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+
+		// 3. Define Predicates etc using Criteria Builder
+
+		// 4. Add Predicates etc to the Criteria Query
+
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+
+		List<Course> resultList = query.getResultList();
+
+		logger.info("Typed Query -> {}", resultList);
+		// [Course[JPA in 50 Steps], Course[Spring in 50 Steps], Course[Spring
+		// Boot in 100 Steps]]
+	}
+}
