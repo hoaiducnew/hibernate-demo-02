83138f5287b3fd0f055cfed4baab3b24e36be09f
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-21 18:58:28 -0400

    101. Step 52 - JPQL - Courses like 100 Steps 102. Step 53 - JPQL - Using Joins

--- src/test/java/com/example/hibernatedemo02/repository/JPQLTest.java
+++ src/test/java/com/example/hibernatedemo02/repository/JPQLTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.hibernatedemo02.HibernateDemoApplication;
 import com.example.hibernatedemo02.entity.Course;
+import com.example.hibernatedemo02.entity.Student;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HibernateDemoApplication.class)
@@ -68,5 +69,51 @@ public class JPQLTest {
 		TypedQuery<Course> query = em.createQuery("Select c from Course c order by size(c.students) desc", Course.class);
 		List<Course> resultList = query.getResultList();
 		logger.info("Results -> {}", resultList);
+	}
+	
+	@Test
+	public void jpql_students_with_passports_in_a_certain_pattern() {
+		TypedQuery<Student> query = em.createQuery("Select s from Student s where s.passport.number like '%1234%'", Student.class);
+		List<Student> resultList = query.getResultList();
+		logger.info("Results -> {}", resultList);
+	}
+	
+	//like
+	//BETWEEN 100 and 1000
+	//IS NULL
+	//upper, lower, trim, length
+	
+	//JOIN => Select c, s from Course c JOIN c.students s
+	//LEFT JOIN => Select c, s from Course c LEFT JOIN c.students s
+	//CROSS JOIN => Select c, s from Course c, Student s
+	//3 and 4 =>3 * 4 = 12 Rows
+	@Test
+	public void join() {
+		Query query = em.createQuery("Select c, s from Course c JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Results Size -> {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course{} Student{}", result[0], result[1]);
+		}
+	}
+
+	@Test
+	public void left_join(){
+		Query query = em.createQuery("Select c, s from Course c LEFT JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Results Size -> {}", resultList.size());
+		for(Object[] result:resultList){
+			logger.info("Course{} Student{}", result[0], result[1]);
+		}
+	}
+
+	@Test
+	public void cross_join(){
+		Query query = em.createQuery("Select c, s from Course c, Student s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Results Size -> {}", resultList.size());
+		for(Object[] result:resultList){
+			logger.info("Course{} Student{}", result[0], result[1]);
+		}
 	}
 }
