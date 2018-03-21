7d6b5cca35d59f7e5850fcc4c072f519f2638fcc
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-21 13:09:58 -0400

    95. Step 46 - JPA Inheritance Hierarchies and Mappings - Table Per Class

--- src/main/java/com/example/hibernatedemo02/entity/Employee.java
+++ src/main/java/com/example/hibernatedemo02/entity/Employee.java
@@ -1,7 +1,6 @@
 package com.example.hibernatedemo02.entity;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,8 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="EmployeeType")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
 
 	@Id
