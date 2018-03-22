93346b609bc067be6525caa5e53d52b521639015
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-21 22:59:19 -0400

    114. Step 65 - Testing the Spring Data JPA Repository with findById

---
+++ src/test/java/com/example/hibernatedemo02/repository/CourseSpringDataRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.hibernatedemo02.repository;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.hibernatedemo02.HibernateDemoApplication;
+import com.example.hibernatedemo02.entity.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateDemoApplication.class)
+public class CourseSpringDataRepositoryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	CourseSpringDataRepository repository;
+
+	@Test
+	public void findById_CoursePresent() {
+		Optional<Course> courseOptional = repository.findById(10001L);
+		assertTrue(courseOptional.isPresent());
+	}
+
+	@Test
+	public void findById_CourseNotPresent() {
+		Optional<Course> courseOptional = repository.findById(20001L);
+		assertFalse(courseOptional.isPresent());
+	}
+
+}
